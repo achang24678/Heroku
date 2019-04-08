fc9577a6c3869d96c2501bc52d4706c4e5fd8eda
Allen Chang <achang24678@gmail.com>
2019-04-07 20:06:48 -0700

    babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (env) => {
   const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
-    entry: ['babel-polyfill', './src/app.js'],
+    entry: ['babel-polyfill', './src/app.js'], //add babel-polyfill for other browsers compatibility
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
